28f68143b5c1ddc0f24e34360ff01187f44120bc
user <bjohnso@student.wethinkcode.co.za>
2019-05-24 13:33:46 +0200

    deploy updated stable

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production'){
     module.exports = {
-        mongoURI:`mongodb+srv://bob:123@vidjot-prod-blw7r.gcp.mongodb.net/test?retryWrites=true`
+        mongoURI:`mongodb+srv://bob:${encodeURIComponent('Th3sund@33xp')}@vidjot-prod-blw7r.gcp.mongodb.net/test?retryWrites=true`
     }
 } else {
     module.exports = { 
